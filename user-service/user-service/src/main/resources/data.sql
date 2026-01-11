07dc1de2c1466a9e133429ad676a290594c54fd2
ashutosh25apr <ashutosh25apr@gmail.com>
2026-01-11 12:23:00 +0000

    removed duplicate

--- user-service/user-service/src/main/resources/data.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO users (name, username, password, email, roles) VALUES ('Test User', 'testuser', 'testpassword', 'test.test@email.com', 'ADMIN');
--- user-service/user-service/src/test/resources/data.sql
+++
@@ -1,3 +0,0 @@
--- this script is used by the UserRepositoryIntegrationTest
-
-INSERT INTO users (name, username, password, email, roles) VALUES ('Colin But', 'colinbut', 'password', 'colin.but@outlook.com', 'ADMIN');
